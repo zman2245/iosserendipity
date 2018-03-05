b923f7d68da2f3b4bff39a0c2af8512b298c7cab
Zachary Foster <zfoster22@gmail.com>
2018-03-05 17:36:23 -0500

    Initial Commit

---
+++ Serendipity/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Serendipity
+//
+//  Created by Zachary Foster on 3/5/18.
+//  Copyright © 2018 Big Z Industries, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
