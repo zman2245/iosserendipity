8a8424c6b7dc562fecefdad37bb97b874a03a633
Zachary Foster <zfoster22@gmail.com>
2018-05-30 11:42:26 -0300

    Create Controllers group

---
+++ Serendipity/Controllers/HotspotListController.swift
@@ -0,0 +1,21 @@
+//
+//  HotspotListController.swift
+//  Serendipity
+//
+//  Created by Zachary Foster on 5/30/18.
+//  Copyright © 2018 Big Z Industries, LLC. All rights reserved.
+//
+
+import UIKit
+
+class HotspotListController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell();
+    }
+    
+    
+}
